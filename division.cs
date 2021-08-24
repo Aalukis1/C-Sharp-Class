b16dbdee60b55d7e1e777f12d7009f8c269fd845
Aliyu <46341771+Aalukis1@users.noreply.github.com>
2021-08-25 00:04:41 +0100

    Add files via upload

---
+++ division.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+namespace Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // program to divide numbers
+            int num1 = 0;
+            int num2 = 0;
+            int result = 0;
+
+            //collect values from user
+            Console.Write("please input your first number: ");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("please input the first number: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            result = num1/num2;
+            Console.WriteLine("The answer is: " + result);
+        }
+    }
+}
