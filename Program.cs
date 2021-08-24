9c91db2ffa5b6b88087604a4e9cf96a7054691b5
Aliyu <46341771+Aalukis1@users.noreply.github.com>
2021-08-24 23:50:55 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,26 +1,32 @@
 ﻿
+
 using System;
 
-namespace Division
+namespace multiplication
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // program to divide numbers
+            // program to multipliply 3 digits
             int num1 = 0;
             int num2 = 0;
+            int num3 = 0;
             int result = 0;
 
             //collect values from user
-            Console.Write("please input your first number: ");
+            Console.Write("please enter the first number: ");
             num1 = int.Parse(Console.ReadLine());
 
-            Console.Write("please input the first number: ");
+            Console.Write("please enter your second number: ");
             num2 = int.Parse(Console.ReadLine());
 
-            result = num1/num2;
-            Console.WriteLine("The answer is: " + result);
+            Console.Write("please enter the third number: ");
+            num3 = int.Parse(Console.ReadLine());
+
+            //multiply the 3 numbers
+            result = num1 * num2 * num3;
+            Console.WriteLine("The product of the numbers is: " + result);
         }
     }
 }
